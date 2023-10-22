2108abb64a1889be1edb33da1bc6d0d83f4f905b
Clement127001 <babiyonclement18@gmail.com>
2023-10-22 13:21:53 +0530

    add post route

--- 11-working-with-methods.js
+++ 11-working-with-methods.js
@@ -11,16 +11,35 @@ const { people } = require("./data");
 //midlleware for static assets and handling the request body
 app.use(express.static("./methods-public"));
 
+//for parsing the form data
 app.use(express.urlencoded({ extended: false }));
+
+//for parsing json
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  console.log(name);
+
+  if (!name)
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "please provide the valid name" });
+
+  //the data is obtained using the reponse that is send by the post request, handled via the variable used
+  res.status(201).json({ success: true, person: name });
+  //   res.status(201).send("The data is obtained");
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
 
-  if (name) res.status(200).send(`welcome ${name}`);
+  if (name) res.status(200).json({ name });
 
   res.status(401).send("Please provide the user details");
 });
